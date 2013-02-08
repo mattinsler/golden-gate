368d731e145369dc946edfd882110a5720daad6b
Matt Insler <matt.insler@gmail.com>
2013-02-08 12:13:40 -0800

    sound automation

--- javascripts/bulkhead.js
+++ javascripts/bulkhead.js
@@ -1,6 +1,9 @@
 var office_root = 'http://tenderloin-sf.herokuapp.com/';
 // var office_root = 'http://localhost:3000/';
 
+golden_gate = window.golden_gate = {};
+
+// Bulkhead
 
 var Bulkhead = function() {
   this.microphone_button = $('[role="button"][aria-label^="Microphone"]');
@@ -49,10 +52,40 @@ Bulkhead.prototype.camera_off = function() {
   }
 };
 
+var is_url_rx = new RegExp('^https?://');
+
 Bulkhead.prototype.play_sound = function(filename) {
-  new buzz.sound(office_root + 'sounds/' + filename, {autoplay: true});
+  if (!golden_gate.sounds) { golden_gate.sounds = {}; }
+  
+  if (!is_url_rx.test(filename)) {
+    filename = office_root + 'sounds/' + filename;
+  }
+  var sound = new buzz.sound(filename);
+  var current = golden_gate.sounds.current_sound;
+  
+  if (current) {
+    current.fadeOut(2000, function() {
+      golden_gate.sounds.current_sound = sound;
+      sound.play();
+    });
+  } else {
+    golden_gate.sounds.current_sound = sound;
+    sound.play();
+  }
+  
+  sound.bind('ended', function() {
+    delete golden_gate.sounds.current_sound;
+  });
 };
 
+Bulkhead.prototype.stop_sound = function() {
+  if (golden_gate.sounds && golden_gate.sounds.current_sound) {
+    golden_gate.sounds.current_sound.fadeOut(2000);
+    delete golden_gate.sounds.current_sound;
+  }
+}
+
+// Bulkhead: End
 
 var mouse_event = function(name, target) {
   var evt = document.createEvent('MouseEvents');
@@ -76,7 +109,12 @@ var connect = function(office) {
   
   if (!office || office === '') { throw new Error('Must supply an office name'); }
   
-  var socket = io.connect(office_root + office);
+  if (golden_gate.socket) {
+    golden_gate.socket.disconnect();
+    golden_gate.socket = null;
+  }
+  
+  var socket = golden_gate.socket = io.connect(office_root + office);
   
   socket.on('connect', function() {
     console.log('Connected to Tenderloin!');
@@ -101,14 +139,3 @@ chrome.storage.local.get('office', function(data) {
     connect(data.office);
   }
 });
-// 
-// $('body').append('<script src="http://localhost:3000/socket.io/socket.io.js"></script>');
-// 
-// socket = io.connect('http://localhost:3000/sf');
-// 
-// ['connect', 'connecting', 'error', 'anything', 'message', 'disconnect'].forEach(function(e) {
-//   socket.on(e, function() {
-//     console.log(e);
-//     console.log(arguments);
-//   });
-// });
